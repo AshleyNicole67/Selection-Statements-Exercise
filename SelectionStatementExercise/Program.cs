1c2a9698a445c12631fe3881adc3105d55ce8aa1
Ashley Doering <Ashley.doering@outlook.com>
2025-06-30 22:33:46 -0400

    I added a coded sequence where we can play a number guessing game till we win

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace SelectionStatementExercise
+﻿using System.ComponentModel.Design;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var random = new Random();
+            var favNumber  = random.Next(1, 11);
+            int response = 0;
+
+            while (response != favNumber)
+            {
+                Console.WriteLine("Give me a number:");
+                response = int.Parse(Console.ReadLine());
+
+                if (response < favNumber)
+                {
+                    Console.WriteLine($"Too low!\nYour guess: {response}");
+                }
+                else if (response > favNumber)
+                {
+                    Console.WriteLine($"Too High!\nYour guess: {response}");
+                }
+                else 
+                {
+                    Console.WriteLine($"You guessed it!\nThe favorite number was {favNumber}!");
+                } 
+            }
+
+           
+
+
         }
+        
     }
 }
